b75cb7878f87aa52b5f9c2155afb1d6c6a7c683c
Amrutha K <59998215+amruthamuralikt@users.noreply.github.com>
2020-07-09 14:57:09 +0530

    Update insert_data.php

--- api/insert_data.php
+++ api/insert_data.php
@@ -1,26 +1,32 @@
 <?php
-require '..../dbconfig.php';
-$inf = file_get_contents("php://input");
-$info = json_decode($inf);
-
+$conn = mysqli_connect("localhost","root","","ang_post");
+$info = (array)json_decode(file_get_contents("php://input"));
 if(count($info) > 0)
-//if(isset($_POST['insert']))
+{   $name = mysqli_real_escape_string($conn, $info['name']);
+    $age = mysqli_real_escape_string($conn,  $info['email']);
+    $email = mysqli_real_escape_string($conn,  $info['age']);
+    $time = mysqli_real_escape_string($conn,  $info['time']);
+    $query = "INSERT INTO time_ticker (name, email, age, time) VALUES ('$name', '$email', '$age','$time')";
+    if(mysqli_query($conn,$query)) {
+        echo "Data inserted succesfully";
+       
+    }
+    else{
+        echo "Data not insterted";
+    }
 
+    $re="SELECT * FROM time_ticker WHERE name='$name'";
+$result=mysqli_query($conn,$re);
+while($res=mysqli_fetch_assoc($result))
 {
-$name = mysqli_real_escape_string($mysqli, $info->name);
-$email = mysqli_real_escape_string($mysqli, $info->email);
-$age = mysqli_real_escape_string($mysqli, $info->age);
+  echo "<br><br> <b>Name</b>: " . $res['name']."<br><br>";   
+         
+  echo "<b>Contact Number</b>: " . $res['email']."<br><br>";
+ 
+  echo "<b>Email</b>: " . $res['age']."<br><br>";
 
-$sql = "INSERT INTO time_ticker (name,email,age) VALUES ('$name','$email','$age')";
-if (mysqli_query($mysqli,$sql)) 
-{
-    echo "New record created successfully";
-  } 
-  else
-   {
-    echo "Error: " . $sql . "<br>" . $mysqli->error;
-  }
+  echo "<b>Email</b>: " . $res['time']."<br><br>";
+ 
 }
-
-$mysqli -> close();
+}
 ?>
